7fa81a593e35f51d9e5b8dba2e29d9def10ab1e0
Ben Simpson <b.albertsimpson@gmail.com>
2020-04-21 02:42:17 -0700

    updates to palindromes

--- palindromes.py
+++ palindromes.py
@@ -1,6 +1,7 @@
 #!python
 
 import string
+import re
 # Hint: Use these string constants to ignore capitalization and/or punctuation
 # string.ascii_lowercase is 'abcdefghijklmnopqrstuvwxyz'
 # string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -13,23 +14,44 @@ def is_palindrome(text):
     # implement is_palindrome_iterative and is_palindrome_recursive below, then
     # change this to call your implementation to verify it passes all tests
     assert isinstance(text, str), 'input is not a string: {}'.format(text)
-    return is_palindrome_iterative(text)
-    # return is_palindrome_recursive(text)
+    # return is_palindrome_iterative(text)
+    return is_palindrome_recursive(text)
 
 
 def is_palindrome_iterative(text):
     # TODO: implement the is_palindrome function iteratively here
-    pass
     # once implemented, change is_palindrome to call is_palindrome_iterative
     # to verify that your iterative implementation passes all tests
 
+    left = 0
+    right = len(text) - 1
+
+    while left <= right:
+        if text[left] == text[right]:
+            left += 1
+            right -= 1
+        else:
+            return False
+    return True
 
 def is_palindrome_recursive(text, left=None, right=None):
     # TODO: implement the is_palindrome function recursively here
-    pass
     # once implemented, change is_palindrome to call is_palindrome_recursive
     # to verify that your iterative implementation passes all tests
 
+    if left == None and right == None:
+        left = 0
+        right = len(text) - 1
+
+    if left >= right:
+        return True
+
+    if text[left] is not text[right]:
+        return False
+    else:
+        left = left + 1
+        right = right - 1
+        return is_palindrome_recursive(text)
 
 def main():
     import sys
